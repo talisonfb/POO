fa5158b38306011c93ba32dac2afdd2aee8f7b4f
Talison Ferreira Brisola <Talison_Brisola@0thwz.onmicrosoft.com>
2022-08-28 17:03:04 -0300

    OK

---
+++ TermoThis/Acessar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{
+    internal class Acessar
+    {
+        private string senha = "abc123";
+
+        public bool Login(string senha)
+        {
+            return senha == this.senha;
+        }
+    }
+}
---
+++ TermoThis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acessar a = new Acessar();
+
+            if (a.Login("abfd"))
+            {
+                Console.WriteLine("Seja bem-vindo.");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado");
+            }
+        }
+    }
+}
