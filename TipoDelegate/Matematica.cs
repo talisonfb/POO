6771a8f8e35f1d7e03626852e6cd588b44340701
Talison Ferreira Brisola <Talison_Brisola@0thwz.onmicrosoft.com>
2023-04-23 15:51:32 -0300

    ok

---
+++ TipoDelegate/Matematica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDelegate
+{
+    internal class Matematica
+    {
+        public void Somar(int n1, int n2)
+        {
+            int total = n1 + n2;
+            Console.WriteLine($"A soma é {total}");
+        }
+
+        public void Subtrair(int n1, int n2)
+        {
+            int total = n1 - n2;
+            Console.WriteLine($"O total é {total}");
+        }   
+
+        public void Multiplicar(int n1, int n2)
+        {
+            int total = n1 * n2;
+            Console.WriteLine($"O total é {total}");
+        }
+
+        public void Dividir(int n1, int n2)
+        {
+            int total = n1 / n2;
+            Console.WriteLine($"O total é {total}");
+        }
+    }
+}
--- TipoDelegate/Program.cs
+++ TipoDelegate/Program.cs
@@ -8,8 +8,21 @@ namespace TipoDelegate
 {
     internal class Program
     {
+        delegate void Operacao(int num1, int num2);
+
         static void Main(string[] args)
         {
+            Matematica m = new Matematica();
+
+            Operacao conta = null;
+
+            conta += m.Multiplicar;
+            conta += m.Somar;
+            conta += m.Dividir;
+            conta += m.Subtrair;
+
+            conta(10, 2);
+
         }
     }
 }
